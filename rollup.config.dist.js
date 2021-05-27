46921a698d87abf6c268731214b90e02e83ba3b3
xeolabs <lindsay.kay@xeolabs.com>
2021-05-27 10:31:32 +0200

    Rollup config

--- rollup.config.dist.js
+++ rollup.config.dist.js
@@ -1,4 +1,5 @@
 import nodeResolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
 import minify from 'rollup-plugin-minify-es';
 
 export default {
@@ -6,17 +7,23 @@ export default {
     output: [
         {
             file: './dist/xeokit-xkt-utils.es.js',
+            include: '/node_modules/',
             format: 'es',
             name: 'bundle'
-        },
+        }        ,
         {
             file: './dist/xeokit-xkt-utils.cjs.js',
+            include: '/node_modules/',
             format: 'cjs',
             name: 'bundle2'
         }
     ],
     plugins: [
-        nodeResolve(),
-        minify()
+        nodeResolve({
+            browser: true,
+            preferBuiltins: false
+        }),
+        commonjs(),
+        // minify()
     ]
 }
