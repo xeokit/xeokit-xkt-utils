585057f2797387eebe29d1afe63c92528f20466f
xeolabs <lindsay.kay@xeolabs.com>
2021-06-18 20:48:00 +0200

    Extend Percy tests to test convert2xkt

--- snapshots.js
+++ snapshots.js
@@ -1,54 +1,98 @@
+#!/usr/bin/env node
+
 const PercyScript = require('@percy/script');
 const httpServer = require('http-server');
+const convert2xkt = require("./dist/convert2xkt.cjs.js");
+const fs = require('fs');
+const rimraf = require("rimraf");
+const path = require("path");
 
-PercyScript.run(async (page, percySnapshot) => {
+const SOURCE_FILES = [
+    "tests/models/3dxml/widget.3dxml",
+    "tests/models/cityjson/csol.json",
+    "tests/models/cityjson/cube.json",
+    "tests/models/cityjson/DenHaag_01.json",
+    "tests/models/cityjson/LoD3_Railway.json",
+    "tests/models/ifc/Duplex.ifc",
+    "tests/models/ifc/IfcOpenHouse2x3.ifc",
+    "tests/models/ifc/IfcOpenHouse4.ifc",
+    "tests/models/ifc/MAP.ifc",
+    "tests/models/ifc/rac_advanced_sample_project.ifc",
+    "tests/models/ifc/rme_advanced_sample_project.ifc"
+];
 
-    async function testPage(pageName) {
+const OUTPUT_DIR = "./tests/models/xkt";
+
+// PercyScript.run(async (page, percySnapshot) => {
+//     await test();
+// });
+
+
+test().catch(err => {
+    console.error('Error:', err);
+    process.exit(1);
+});
+
+async function test() {
+
+    async function convert(source) {
+
+        const dir = "./tests/models/xkt/" + path.basename(source, path.extname(source))
+        const propsDir = dir + "/props";
+
+        if (!fs.existsSync(dir)) {
+            fs.mkdirSync(dir);
+        }
+
+        if (!fs.existsSync(propsDir)) {
+            fs.mkdirSync(propsDir);
+        }
+
+        await convert2xkt({
+
+            source,
+
+            outputXKT: async function (xktData) {
+                await fs.promises.writeFile(dir + "/model.xkt", xktData);
+            },
+
+            outputObjectProperties: async function (id, props) {
+                await fs.promises.writeFile(propsDir + "/" + id + ".json", JSON.stringify(props, null, "\t"));
+            },
+
+            log: (msg) => {
+                console.log(msg)
+            }
+        });
+    }
+
+    async function testPage(source) {
+
+        const dir = "./tests/models/xkt/" + path.basename(source, path.extname(source))
+        const propsDir = dir + "/props";
+        const xktFilePath = dir + "/model.xkt";
         await page.goto('http://localhost:3000/tests/' + pageName);
         await page.waitForFunction(() => !!document.querySelector('#percyLoaded'));
         await percySnapshot(pageName, {
             widths: [1280]
         });
+
     }
 
-    let server = httpServer.createServer();
+    // Create XKT files and property sets
 
-    server.listen(3000);
+    rimraf.sync(OUTPUT_DIR);
+    fs.mkdirSync(OUTPUT_DIR);
 
-    console.log(`Server started`);
+    for (let i = 0, len = SOURCE_FILES.length; i < len; i++) {
+        await convert(SOURCE_FILES[i]);
+    }
 
-    await testPage('convert_glTF_doublePrecision_MAP.html');
-    await testPage('convert_glTF_Duplex.html');
-    await testPage('convert_glTF_IfcOpenHouse2x3.html');
-    await testPage('convert_glTF_IfcOpenHouse4.html');
-    await testPage('convert_glTF_lines.html');
-    await testPage('convert_glTF_MAP.html');
-    await testPage('convert_glTF_PBR_GearboxPropellerTurbine.html');
-    await testPage('convert_glTF_PBR_ReciprocatingSaw.html');
-    await testPage('convert_glTF_pointCloud_MAP.html');
-    await testPage('convert_glTF_Schependomlaan.html');
-    await testPage('generate_batching_lines.html');
-    await testPage('generate_batching_PBR_metallicVsRoughness.html');
-    await testPage('generate_batching_points.html');
-    await testPage('generate_batching_stairCase.html');
-    await testPage('generate_batching_triangles.html');
-    await testPage('generate_buildBoxGeometry.html');
-    await testPage('generate_buildBoxLinesGeometry.html');
-    await testPage('generate_buildCylinderGeometry.html');
-    await testPage('generate_buildGeometries.html');
-    await testPage('generate_buildGridGeometry.html');
-    await testPage('generate_buildPlaneGeometry.html');
-    await testPage('generate_buildSphereGeometry.html');
-    await testPage('generate_buildTorusGeometry.html');
-    await testPage('generate_buildVectorTextGeometry.html');
-    await testPage('generate_instancing_batching_triangles.html');
-    await testPage('generate_instancing_lines.html');
-    await testPage('generate_instancing_PBR_metallicVsRoughness.html');
-    await testPage('generate_instancing_points.html');
-    await testPage('generate_instancing_stairCase.html');
-    await testPage('generate_instancing_triangles.html');
-    await testPage('generate_metaModel.html');
+    // Make visual snapshot of each model
 
-    server.close();
-});
+    // for (let i = 0, len = SOURCE_FILES.length; i < len; i++) {
+    //     await testPage("loadXKT.html?xkt_src=../" + SOURCE_FILES[i]);
+    // }
 
+    console.log("Done.");
+}
