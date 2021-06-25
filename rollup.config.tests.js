6c49d170105ea3d6923ec3e26b78c87a8e806293
xeolabs <lindsay.kay@xeolabs.com>
2021-06-25 13:30:31 +0200

    Rollup fix

--- rollup.config.tests.js
+++ rollup.config.tests.js
@@ -3,7 +3,7 @@ import nodeResolve from 'rollup-plugin-node-resolve';
 export default {
     input: "@xeokit/xeokit-sdk/dist/xeokit-sdk.es.js",
     output: {
-        file: './assets/libs/xeokit-sdk.es.js',
+        file: './assets/lib/xeokit-sdk.es.js',
         format: 'es',
         name: 'bundle'
     },
